802285866ef757c1ba6c0bc828833dc921e7c648
Michin <ubaldoabi9@gmail.com>
2026-04-14 14:50:15 -0600

    calculadora2.c

--- calculadora2.c
+++ calculadora2.c
@@ -1,28 +1,87 @@
-#include <stdio.h> 
-int main () 
-{ 
-    int op, uno, dos; 
-    do 
+#include <stdio.h>
+#include <math.h>
+
+//declaración de variables globales
+//declaración de fucniones 
+double suma1(double n1, double n2)
+{
+    double r;
+    r = n1 + n2;
+    return r;
+}
+
+void suma2(double n1, double n2)
+{
+    double r;
+    r = n1 + n2;
+    printf("El resultado de la suma es %lf\n", r);
+}
+
+
+void main()
+{
+    double num1, num2, res;
+    int op;
+
+    do
     { 
         printf(" --- Calculadora ---\n"); 
         printf("\n¿Qué desea hacer\n"); 
-        printf("1) Sumar\n"); 
-        printf("2) Restar\n"); 
-        printf("3) Multiplicar\n"); 
-        printf("4) Dividir\n"); 
-        printf("5) Salir\n"); 
+        printf("1) Suma\n"); 
+        printf("2) Resta\n"); 
+        printf("3) Multiplicación\n"); 
+        printf("4) Dividición\n");
+        printf("5) Raíz cuadrada\n");
+        printf("6) Potencia\n");
+        printf("5) Salida\n"); 
         scanf("%d",&op); 
-        switch(op) 
+        do 
         {
-           case 1: 
-                printf("\tSumar\n"); 
-                printf("Introduzca los números a sumar separados por comas\n"); 
-                scanf("%d, %d",&uno, &dos); 
-                printf("%d + %d = %d\n", uno, dos, (uno + dos)); 
-                break; 
-            case 2: 
-                printf("\tRestar\n"); 
-                printf("Introduzca los números a restar separados por comas\n"); 
-                scanf("%d, %d",&uno, &dos); 
-                printf("%d - %d = %d\n", uno, dos, (uno - dos)); 
-                break;
+                printf("Opción a escoger: \n"); 
+                scanf("%d", &op); 
+        }
+        while(op < 1 || op > 7);
+
+        if(op !=7)
+        {
+            if(op == 5)
+            {
+                printf("Dame la cifra para la raíz: ");
+                scanf("%lf", &num1); 
+                if(num1 < 0)
+                {
+                    do
+                    {
+                        printf("Número no válido, ingresar otro número: ");
+                        scanf("%lf", &num1);
+                    }
+                    while(num1 < 0);
+                }
+            }
+            else
+            {
+                if(op == 4)
+                {
+                    printf("Dame la primer cifra: ");
+                    scanf("%lf", num1);
+                    printf("Dame la segunda cifra: ");
+                    scanf("%lf", num2);
+                    if(num2 == 0);
+                    {
+                        do
+                        {
+                            printf("No es posible dividir entre cero, ingresar otro número: ");
+                            scanf("%lf", &num2);
+                        }
+                        while(num1 == 0);
+                    }
+                }
+                else
+                {
+                    if(op == 6)
+                    {
+                        printf("Dame la base: ");
+                        scanf("%lf"
+        }
+                switch(op)
+                {
